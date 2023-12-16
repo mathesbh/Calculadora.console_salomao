9c68296ebfa7c8d826b701d39a3de404c4c2669a
Matheus Salomão <mathesbh@gmail.com>
2023-12-16 11:46:48 -0300

    chore: inclui o restante das operações básicas

--- Application/Operacoes.cs
+++ Application/Operacoes.cs
@@ -6,5 +6,20 @@
         {
             return n1 + n2;
         }
+
+        public static float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        public static float Dividir(float n1, float n2)
+        {
+            return n1 / n2;
+        }
+
+        public static float Multiplicar(float n1, float n2)
+        {
+            return n1 * n2;
+        }
     }
 }
