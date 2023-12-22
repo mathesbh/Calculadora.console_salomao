8445717a669811b71de90783d51b4578b7dd32b7
Matheus Salomão <mathesbh@gmail.com>
2023-12-21 21:17:17 -0300

    chore: inclui a persistência de dados do histórico das operações

---
+++ Data/OperacoesHistoricoDAO.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Application.Models;
+
+namespace Data
+{
+    public class OperacoesHistoricoDAO : IOperacoesHistorico, IDisposable
+    {
+        private readonly CalculadoraDbContext _context;
+
+        public OperacoesHistoricoDAO()
+        {
+            _context = new CalculadoraDbContext();
+        }
+
+        public void Adicionar(OperacoesHistorico historico)
+        {
+            _context.OperacoesHistoricos.Add(historico);
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
