2da4597a9cb07923c449fa605aa2a62af1a1b4fb
Matheus Salomão <mathesbh@gmail.com>
2023-12-21 21:08:23 -0300

    feat: add modelo histórico operações

---
+++ Application/Models/OperacoesHistorico.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Models
+{
+    public class OperacoesHistorico
+    {
+        public OperacoesHistorico(string? operacao, float resultado, DateTime data)
+        {
+            Id = Guid.NewGuid();
+            Operacao = operacao;
+            Resultado = resultado;
+            Data = data;
+        }
+
+        public Guid Id { get; set; }
+        public string? Operacao { get; set; }
+        public float Resultado { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
