2528f32316f896484e1be57b31a4fee37ed0c38b
Matheus Salomão <mathesbh@gmail.com>
2023-12-21 21:13:29 -0300

    chore: inclui mapeamento operações histórico e gera migrations

---
+++ Data/Migrations/20231222000045_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OperacoesHistoricos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Operacao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Resultado = table.Column<float>(type: "real", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperacoesHistoricos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OperacoesHistoricos");
+        }
+    }
+}
---
+++ Data/OperacoesHistoricoMapping.cs
@@ -0,0 +1,14 @@
+﻿using Application.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data
+{
+    public class OperacoesHistoricoMapping : IEntityTypeConfiguration<OperacoesHistorico>
+    {
+        public void Configure(EntityTypeBuilder<OperacoesHistorico> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
