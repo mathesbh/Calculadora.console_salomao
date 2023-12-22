fe9315a810f9d4da0b6db6a4c762c26caad507b7
Matheus Salomão <mathesbh@gmail.com>
2023-12-21 21:10:25 -0300

    chore: inclui db context

---
+++ Data/CalculadoraDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CalculadoraDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=calculadora_frwk;User ID=sa;Password=1q2w3e4r@#$");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CalculadoraDbContext).Assembly);
+        }
+
+        public DbSet<OperacoesHistorico> OperacoesHistoricos { get; set; }
+    }
+}
